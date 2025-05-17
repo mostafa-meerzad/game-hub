6956600045a39e4499bb46e780bd139845fa646a
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-05-17 13:50:46 +0430

    display platform icons

--- src/hooks/useFetchGames.ts
+++ src/hooks/useFetchGames.ts
@@ -2,10 +2,18 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: {platform: Platform}[]
 }
 
 interface FetchGamesResponse {
@@ -66,6 +74,7 @@ const useFetchGames = () => {
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        console.log(res.data.results);
       })
       .catch((err) => {
         if (err instanceof CanceledError) return;
