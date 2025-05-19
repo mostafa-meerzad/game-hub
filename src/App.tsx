b2ac53a50ada89247888bc4578ce8dcb80fd9884
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-05-19 12:25:50 +0430

    highlight the selected genre

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
         area={"aside"}
         paddingX={5}
       >
-        <GenreList onSelectGenre={(genre) => setSelectedGenre(genre)} />
+        <GenreList selectedGenre={selectedGenre} onSelectGenre={(genre) => setSelectedGenre(genre)} />
       </GridItem>
       <GridItem area={"main"}>
         <GameGrid selectedGenre={selectedGenre} />
