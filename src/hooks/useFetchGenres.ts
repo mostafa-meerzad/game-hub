e9499971dc5e4e9f39d7e7faba04da023ad39006
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-05-22 20:07:01 +0430

    fix genre list type compatibility issue

--- src/hooks/useFetchGenres.ts
+++ src/hooks/useFetchGenres.ts
@@ -1,11 +1,12 @@
 import genres from "../data/genres";
 
 export interface Genre {
-  id: string;
+  id: number;
   name: string;
   image_background: string;
 }
 
-const useFetchGenres = () => ({ data: genres, isLoading: false, error: null });
+const useFetchGenres = () => ({ data: genres, loading: false, error: null });
 
 export default useFetchGenres;
+"compatibility"
