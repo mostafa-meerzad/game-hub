759490bd1e2d5a930486bafd08a0b60cb9ce3aa0
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-01 21:27:48 +0430

    install and configure react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,28 @@ import "./index.css";
 import App from "./App.tsx";
 import theme from "./theme.tsx";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchInterval: 24 * 60 * 60 * 1000,// 24h
+      refetchOnMount: false,
+      refetchOnReconnect: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </StrictMode>
 );
