4203b707cbdfe20e384a0281cbe070d1643da140
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-12 18:14:57 +0430

    build expandable text

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,18 +1,32 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import {
+  Button,
+  Collapse,
+  Heading,
+  Spinner,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+  const { isOpen, onToggle } = useDisclosure();
 
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
+
   return (
     <>
       <Heading>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <Collapse startingHeight={70} in={isOpen}>
+        <Text noOfLines={isOpen ? undefined : 3}>{game.description_raw}</Text>
+      </Collapse>
+      <Button onClick={onToggle} size={"sm"} mt={2} variant={"solid"}>
+        {isOpen ? "Show Less" : "Read More"}
+      </Button>
     </>
   );
 };
