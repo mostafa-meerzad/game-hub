41b62c82791ccb194c25784edfe27754de36c7a8
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-12 17:45:38 +0430

    fetch game details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,11 +1,21 @@
 import { Box } from "@chakra-ui/react";
+import { motion } from "framer-motion";
 import { PropsWithChildren } from "react";
 
+const MotionBox = motion(Box);
 const GameCardContainer = ({ children }: PropsWithChildren) => {
   return (
-    <Box  borderRadius={10} overflow={"hidden"}>
+    <MotionBox
+      borderRadius={10}
+      overflow={"hidden"}
+      whileHover={{ scale: 1.05 }}
+      transition={{ duration: 0.2 }}
+      boxShadow="md"
+      _hover={{ boxShadow: "xl" }}
+      cursor="pointer"
+    >
       {children}
-    </Box>
+    </MotionBox>
   );
 };
 
---
+++ src/hooks/useGame.ts
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Game } from "./useFetchGames";
+
+const apiClient = new APIClient<Game>("/games");
+
+const useGame = (slug: string) =>
+  useQuery({
+    queryKey: [slug],
+    queryFn: () => apiClient.get(slug),
+  });
+
+export default useGame;
