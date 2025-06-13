e9f034ab7cd73b117c62c7fc603e61aa2d4f5501
Mostafa Meerzad <mostafameerzad@gmail.com>
2025-06-13 22:32:51 +0430

    refactor- add search button on the search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,8 +1,8 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { IconButton, Input, InputGroup, InputLeftElement, InputRightElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
-import useQueryStore from "../store";
 import { useNavigate } from "react-router-dom";
+import useQueryStore from "../store";
 
 const SearchInput = () => {
   const setSearchText = useQueryStore((s) => s.setSearchText);
@@ -20,7 +20,7 @@ const SearchInput = () => {
       }}
     >
       <InputGroup>
-        <InputLeftElement children={<BsSearch />} />
+        {/* <InputLeftElement children={<BsSearch />} color={"gray.600"}/> */}
 
         <Input
           borderRadius={20}
@@ -28,6 +28,16 @@ const SearchInput = () => {
           variant={"filled"}
           ref={searchRef}
         />
+        <InputRightElement width="3rem">
+          <IconButton
+            aria-label="Search games"
+            icon={<BsSearch />}
+            size="md"
+            variant="ghost"
+            borderRadius={"full"}
+            type="submit"
+          />
+        </InputRightElement>
       </InputGroup>
     </form>
   );
